58305ca6e5d7b94b083be0905c4017927588544d
unknown <brucelukyanov@gmail.com>
2024-10-04 11:20:02 +0000

    Exercise 5 done

--- src/App.js
+++ src/App.js
@@ -1,19 +1,21 @@
-import './App.css';
-//imported components
-import Content from './components/content';
-import Footer from './components/footer';
-import Header from './components/header';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'; 
+import NavigationBar from './components/navbar'; // Importing the NavigationBar component
+import Header from './components/header'; // Importing the Header component
+import Content from './components/content'; // Importing the Content component
+import Footer from './components/footer'; // Importing the Footer component
 
 function App() {
   return (
-    //composed Content, Footer and Header from components in return block
-    <div>
-      
-      <Content></Content>
-      <Footer> </Footer>
-      <Header></Header>
-    </div>
+    <Router> {/* Enabling the app in Router to enable routing */}
+      <NavigationBar /> 
+      <Routes> 
+        <Route path="/home" element={<Content />} /> 
+        <Route path="/read" element={<Header />} /> 
+        <Route path="/create" element={<Footer />} /> 
+      </Routes>
+      <Footer /> 
+    </Router>
   );
 }
 
-export default App;
+export default App; // Exporting the App component for use in other parts of the application
