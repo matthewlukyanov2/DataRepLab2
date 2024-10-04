f0d2f2ccdac5d0cb58c762c8f5c68add498885a0
unknown <brucelukyanov@gmail.com>
2024-10-04 10:31:17 +0000

    Excericse 2 done

--- src/App.js
+++ src/App.js
@@ -2,10 +2,10 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  let x =5;
   return (
-    <div className="App">
-      gggg
+    <div>
+      <h1>Hello World!</h1>
+      <h2>It is {new Date().toLocaleTimeString()}.</h2>
     </div>
   );
 }
